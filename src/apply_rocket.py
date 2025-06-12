3fccda97af2502a7414ff49f546af06140a27ba9
Levan Bokeria <levan.bokeria@gmail.com>
2025-06-12 13:13:24 +0100

    ran rocket saving results

--- src/apply_rocket.py
+++ src/apply_rocket.py
@@ -13,8 +13,15 @@ def apply_rocket(train_dict, val_dict, position='FWD',rocket_model='rocket'):
     val = val_dict[position]
 
     # Select relevant columns
-    train = train[['minutes_0', 'minutes_1', 'minutes_2', 'minutes_3','out_minutes',]]
-    val   = val[['minutes_0', 'minutes_1', 'minutes_2', 'minutes_3','out_minutes',]]
+
+    # How many minute columns do we have?
+    num_minutes = len([col for col in train.columns if col.startswith('minutes_')])
+
+    print(f"Number of minute columns: {num_minutes}")
+
+    # Select those columns that start with 'minutes_' and the 'out_minutes' column
+    train = train[[f'minutes_{i}' for i in range(num_minutes)] + ['out_minutes']]
+    val   = val[[f'minutes_{i}' for i in range(num_minutes)] + ['out_minutes']]
 
     # Define X_train as first four columns and y_train as the last column
     X_train = train.iloc[:, :-1]
@@ -24,8 +31,8 @@ def apply_rocket(train_dict, val_dict, position='FWD',rocket_model='rocket'):
     y_val = val.iloc[:, -1]    
 
     # Turn X_train into a 2D array
-    X_train_array = X_train.values.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_val_array   = X_val.values.reshape(X_val.shape[0], X_val.shape[1], 1)
+    X_train_array = X_train.values.reshape(X_train.shape[0], X_train.shape[1])
+    X_val_array   = X_val.values.reshape(X_val.shape[0], X_val.shape[1])
 
     # Now fit the regressor
     reg = RocketRegressor(rocket_transform=rocket_model) 
